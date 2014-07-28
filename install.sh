85809372b79b6ba1a497fd864f0cad4efbdba4f6
Steven Silvester <steven.silvester@ieee.org>
2014-07-28 10:39:27 -0500

    Add the rest of the files to the install and remove unused hg-prompt.

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 cp bashrc ~/.bashrc
 cp hg_bash_completion ~
+cp gitignore ~/.gitignore
 cp gitconfig ~/.gitconfig
+cp git-completion.sh ~/.git-completion.sh
 cp hgrc ~/.hgrc
 cp inputrc ~/.inputrc
 cp pdbrc ~/.pdbrc
 cp pdbrc.py ~/.pdbrc.py
 cp pythonrc ~/.pythonrc
 cp pythonrc.py ~/.pythonrc.py
-mkdir -c ~/.config/matplotlib
+mkdir ~/.config
+mkdir ~/.config/matplotlib
 cp matplotlibrc ~/.config/matplotlib
