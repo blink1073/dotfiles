85638d8c685d663af0f992114d2731f432dff99c
Steven Silvester <steven.silvester@ieee.org>
2014-09-14 07:28:56 -0500

    Fix sublime cp commands

--- install.sh
+++ install.sh
@@ -15,6 +15,6 @@ cp matplotlibrc ~/.config/matplotlib
 ipython profile create
 cp ipython_startup.py ~/.ipython/profile_default/startup/
 
-sublime=~/AppData/Roaming/Sublime Text 3/Packages/User
-cp Default\ \(Windows\).sublime-keymap $sublime
-cp Preferences.sublime-settings $sublime
+sublime=~/AppData/Roaming/Sublime\ Text\ 3/Packages/User
+cp Default\ \(Windows\).sublime-keymap "$sublime"
+cp Preferences.sublime-settings "$sublime"
