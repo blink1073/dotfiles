b080f0abf6bf0a4fbcd547df1a25c78bb7357bc6
Steven Silvester <steven.silvester@ieee.org>
2018-09-27 07:23:58 -0500

    update install file

--- install.sh
+++ install.sh
@@ -1,23 +1,19 @@
-cd ~/workspace/dotfiles
-
+set -x
 cp bashrc ~/.bashrc
-cp hub_bash_completion.sh ~/.hub_bash_completion.sh
 cp gitignore ~/.gitignore
 cp gitconfig ~/.gitconfig
-cp hg_bash_completion ~
-cp hgrc ~/.hgrc
 cp inputrc ~/.inputrc
 cp pythonrc ~/.pythonrc
 cp pythonrc.py ~/.pythonrc.py
-mkdir ~/.config
-mkdir ~/.config/matplotlib
+cp pdbrc ~/.pdbrc
+cp pypirc ~/.pypirc
+mkdir -p ~/.config/matplotlib
 cp matplotlibrc ~/.config/matplotlib
+
+code="$HOME/AppData/Roaming/Code/User"
+cp keybindings.json $code
+cp vscode_settings.json $code/settings.json
+
+conda install -y ipython
 ipython profile create
 cp ipython_startup.py ~/.ipython/profile_default/startup/
-
-sublime=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-cp Python.sublime-settings $sublime
-cp Preferences.sublime-settings $sublime
-#cp "Default (Linux).sublime-keymap" $sublime
-cp debug.sublime-snippet $sublime
-cp Anaconda.sublime-settings $sublime
