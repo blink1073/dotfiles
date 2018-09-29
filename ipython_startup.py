51d367440d75cf34824e04336be72bb97f529ff8
Steven Silvester <steven.silvester@ieee.org>
2018-09-29 06:08:14 -0500

    clean up ipython_startup

--- ipython_startup.py
+++ ipython_startup.py
@@ -1,7 +1,5 @@
-# run ipython profile create and put this file in 
+# run ipython profile create and put this file in
 # ~/.ipython/profile_default/startup
 import sys
 import os
-import numpy as np 
-import matplotlib.pyplot as plt
 import re
